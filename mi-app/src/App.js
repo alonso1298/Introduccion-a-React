a2b9f5a611600945a088fe67a70a65e1505a3a01
alonso1298 <alonsosagrerogranados@gmail.com>
2025-03-12 13:28:04 -0600

    Se reutilizan componentes con estilo

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,28 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
 
-const estilo2 = {
-  boxShadow: '0px 5px 3px rgba(0,0,0,0.5)'
-}
-const estilo = ({bg = '#222'}) => ({
-  backgroundColor: bg,
-  color: '#fff',
-  padding: '10px 15px',
-  margin: '10px 15pxs',
-})
-
-const Li = ({ children }) => {
-  return (
-    <li style={{...estilo2, ...estilo({ bg: '#333' })}} className='clase-li'>{children}</li>
-  )
-}
-const  App = () => {
-  const valor = "triste"
-  return (
-    <ul style={estilo({ bg: '#750' })} className='clase-css'>
-      <Li estado="feliz">valor de li</Li>
-    </ul>
-  );
+const App = () => {
+    return(
+        <div>
+            <h1>Hola mundo</h1>
+            <Button onClick={() => console.log('Clickeado')}>
+                Enviar
+            </Button>
+        </div>
+    )
 }
 
-export default App;
+export default App
