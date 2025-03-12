d327df470201cc2703def434ec8038d5c8deec56
alonso1298 <alonsosagrerogranados@gmail.com>
2025-03-11 20:37:57 -0600

    Se agrega estilos a los componentes con inline style

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+const estilo2 = {
+  boxShadow: '0px 5px 3px rgba(0,0,0,0.5)'
+}
+const estilo = ({bg = '#222'}) => ({
+  backgroundColor: bg,
+  color: '#fff',
+  padding: '10px 15px',
+  margin: '10px 15pxs',
+})
+
 const Li = ({ children }) => {
   return (
-    <li className='clase-li'>{children}</li>
+    <li style={{...estilo2, ...estilo({ bg: '#333' })}} className='clase-li'>{children}</li>
   )
 }
 const  App = () => {
   const valor = "triste"
   return (
-    <ul className='clase-css'>
+    <ul style={estilo({ bg: '#750' })} className='clase-css'>
       <Li estado="feliz">valor de li</Li>
     </ul>
   );
