695a93b0c75e658eb28cbab956594a8d5f486a92
alonso1298 <alonsosagrerogranados@gmail.com>
2025-03-12 14:03:33 -0600

    Listas y Keys en react

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,7 +1,12 @@
 import Button from "./Button";
 
+const arr = [
+    'Gtito feliz',
+    'Gatito Triste',
+    'Gatito normal'
+]
 const App = () => {
-    const miVariable = true;
+    const miVariable = false;
 
     if (miVariable) {
         return <p>Mi variable dio True</p>
@@ -9,6 +14,7 @@ const App = () => {
     return(
         <div>
             <h1 onClick={(e) => console.log('Click', e)}>Hola mundo</h1>
+            {arr.map(gatos => <p key={gatos}>{gatos}</p>)}
             <Button onClick={() => console.log('Clickeado')}>
                 Enviar
             </Button>
